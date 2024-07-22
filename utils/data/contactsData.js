04263596ba0430d4c854fc6e16544198846fcba9
unknown <alexli930220@gmail.com>
2024-07-22 18:08:26 -0400

    new2

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,8 +1,8 @@
 export const contactsData = {
-    email: 'alexli930220@gmail.com',
+    email: 'tomsmith09140@gmail.com',
     phone: '+14082562632',
     address: 'Utah, US',
-    github: 'https://github.com/rebert1998',
+    github: 'https://github.com/photoday0914',
     facebook: 'https://www.facebook.com/abusaid.riyaz/',
     linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
     twitter: 'https://twitter.com/said7388',
